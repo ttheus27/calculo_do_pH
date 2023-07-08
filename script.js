3148d688432a882dc0b1f8758868bd9805931bdc
ttheus27 <128244984+ttheus27@users.noreply.github.com>
2023-07-08 09:44:05 -0300

    outra versao do codigo

--- script.js
+++ script.js
@@ -1,65 +1,38 @@
-// Tabela de ácidos fracos
-const tabelaAcidos = {
-  "acido1": {
-    nome: "Ácido Acético",
-    Ka: 1.8e-5
-  },
-  "acido2": {
-    nome: "Ácido Benzóico",
-    Ka: 6.2e-6
-  },
-  "acido3": {
-    nome: "Ácido Cítrico",
-    Ka: 4.0e-7
-  },
-  "acido4": {
-    nome: "Ácido Carbônico",
-    Ka1: 4.3e-7,
-    Ka2: 4.8e-11
-  },
-  "acido5": {
-    nome: "Ácido Fórmico",
-    Ka: 6.3e-6
-  },
-  "acido6": {
-    nome: "Ácido Fosfórico",
-    Ka1: 7.1e-3,
-    Ka2: 6.3e-8,
-    Ka3: 4.8e-13
-  }
-  // Adicione mais ácidos conforme necessário
-};
-
-// Função para calcular o pH do ácido selecionado com base na concentração
+// Função para calcular o pH de um ácido fraco
 function calcularpH() {
-  const acidoSelecionado = document.getElementById("acido").value;
-  const concentracao = parseFloat(document.getElementById("concentracao").value);
+  // Obter os valores dos campos
+  let acido = document.getElementById("acido").value;
+  let concentracao = Number(document.getElementById("concentracao").value);
   
-  if (isNaN(concentracao) || concentracao <= 0) {
-    alert("Por favor, insira uma concentração válida.");
-    return;
+  // Verificar se a concentração é válida
+  if (isNaN(concentracao) || concentracao < 0.1 || concentracao > 20) {
+  // Mostrar uma mensagem de erro
+  document.getElementById("result").innerHTML = "Por favor, digite uma concentração válida entre 0,1 e 20.";
+  return;
   }
-
-  const acid = tabelaAcidos[acidoSelecionado];
-  const nomeAcido = acid.nome;
-
-  let pH;
-
-  if ("Ka" in acid) {
-    const Ka = acid.Ka;
-    const concentracaoMolar = concentracao;
-    const KaConcentrado = Ka * concentracaoMolar;
-    pH = -Math.log10(KaConcentrado);
-  } else if ("Ka1" in acid && "Ka2" in acid && "Ka3" in acid) {
-    const Ka1 = acid.Ka1;
-    const Ka2 = acid.Ka2;
-    const Ka3 = acid.Ka3;
-    const concentracaoMolar = concentracao;
-
-    const H3O_concentrado = Math.sqrt(Ka1 * Ka2 * Ka3) * concentracaoMolar / (Math.sqrt(Ka1 * Ka2) + Math.sqrt(Ka2 * Ka3) + Math.sqrt(Ka3 * Ka1));
-
-    pH = -Math.log10(H3O_concentrado);
+  
+  // Obter os dados do ácido da tabela
+  let tabela = [
+  { nome: "ácido acético", ka: 1.8e-5 },
+  { nome: "ácido benzóico", ka: 6.2e-6 },
+  { nome: "ácido cítrico", ka: 4.0e-7 },
+  { nome: "ácido carbônico", ka: 4.8e-11 },
+  { nome: "ácido fórmico", ka: 6.3e-6 },
+  { nome: "ácido fosfórico", ka: 3.6e-13 }
+  ];
+  
+  // Verificar qual ácido foi escolhido
+  let indice = acido.slice(-1) - 1;
+  
+  // Obter o ácido correspondente à escolha
+  let dados = tabela[indice];
+  
+  // Calcular o valor de x usando a aproximação
+  let x = Math.sqrt(concentracao * dados.ka);
+  
+  // Calcular o pH usando a equação do pH
+  let ph = -Math.log10(x);
+  
+  // Mostrar o resultado na tela
+  document.getElementById("result").innerHTML = `O pH de uma solução de ${dados.nome} ${concentracao} M é ${ph.toFixed(2)}`;
   }
-
-  document.getElementById("result").textContent = `O pH do ${nomeAcido} (${concentracao} mol/L) é ${pH.toFixed(2)}.`;
-}
